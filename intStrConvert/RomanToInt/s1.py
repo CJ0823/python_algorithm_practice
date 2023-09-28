206299eae7369544fec8511662f8201c4db293ba
Changjun Choi <cjchoi@meissa.ai>
2023-09-28 22:06:48 +0900

    RomanToInt done

--- intStrConvert/RomanToInt/s1.py
+++ intStrConvert/RomanToInt/s1.py
@@ -1,17 +1,21 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        ri_map = {"I": 1, "IV": 4, "V": 5, "IX": 9, "X": 10, "XL": 40, "L": 50, "XC": 90, "C": 100, "CD": 400, "D": 500, "CM": 900, "M": 1000}
-        q = ''
-        ans = 0
-        for ch in s:
-            q += ch
-            if q != "I" and q != "X" and q != "C" and q in ri_map.keys():
-                ans += ri_map[q]
-                q = ''
-            elif len(q) == 3:
-                ans += ri_map[q[0]] * 3
-            else:
-                continue
-        return ans
+        m = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
 
-Solution().romanToInt("MCMXCIV")
+        ans = 0
+
+        for i in range(len(s)):
+            if i < len(s) - 1 and m[s[i]] < m[s[i + 1]]:
+                ans -= m[s[i]]
+            else:
+                ans += m[s[i]]
+
+        return ans
