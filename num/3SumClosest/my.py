123caaf7b85150c67facd57b86f3ce05cf7cdb88
Changjun Choi <changjun0823@gmail.com>
2023-10-07 15:30:14 +0900

    3SumClosest my ans update

--- num/3SumClosest/my.py
+++ num/3SumClosest/my.py
@@ -13,23 +13,17 @@ class Solution:
                 new_ans = nums[i] + nums[start] + nums[end]
                 if abs(new_ans - target) < abs(ans - target):
                     ans = new_ans
-                    if new_ans < 0:
+                    if new_ans < target:
                         start += 1
-                    elif new_ans > 0:
+                    elif new_ans > target:
                         end -= 1
                     else:
                         break
                 else:
-                    start += 1
+                    if new_ans < target:
+                        start += 1
+                    elif new_ans > target:
+                        end -= 1
+                    else:
+                        break
         return ans
-
-Solution().threeSumClosest(nums=[-1,2,1,-4], target=1)
-
-# Wrong Case
-'''
-Input
-nums = [4,0,5,-5,3,3,0,-4,-5]
-target = -2
-Output = -4
-Expected = -2
-'''
