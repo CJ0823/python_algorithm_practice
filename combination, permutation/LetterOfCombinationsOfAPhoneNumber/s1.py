237fbeb31150ea6cf3cc723c3f67caf077a88894
Changjun Choi <changjun0823@gmail.com>
2023-10-09 06:36:37 +0900

    backtracking practice

--- combination, permutation/LetterOfCombinationsOfAPhoneNumber/s1.py
+++ combination, permutation/LetterOfCombinationsOfAPhoneNumber/s1.py
@@ -3,19 +3,20 @@ from typing import List
 
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
+        match_map = {
+            '2': ['a', 'b', 'c'], '3': ['d', 'e', 'f'], '4': ['g', 'h', 'i'], '5': ['j', 'k', 'l'],
+            '6': ['m', 'n', 'o'], '7': ['p', 'q', 'r', 's'], '8': ['t', 'u', 'v'], '9': ['w', 'x', 'y', 'z']
+        }
         n = len(digits)
         if n == 0:
             return []
-        dic = {'2': ['a', 'b', 'c'], '3': ['d', 'e', 'f'], '4': ['g', 'h', 'i'], '5': ['j', 'k', 'l'],
-               '6': ['m', 'n', 'o'], '7': ['p', 'q', 'r', 's'], '8': ['t', 'u', 'v'], '9': ['w', 'x', 'y', 'z']}
-        res = []
+        ans = []
 
         def backtracking(r):
             if len(r) == n:
-                res.append(r)
-            else:
-                for i in dic[digits[len(r)]]:
-                    backtracking(r + i)
+                return ans.append(r)
+            for i in match_map[digits[len(r)]]:
+                backtracking(r + i)
 
         backtracking("")
-        return res
+        return ans
