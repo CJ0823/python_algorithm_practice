31e9886390240241b59cac4bacc462aa969f9c11
Changjun Choi <changjun0823@gmail.com>
2023-10-14 10:34:14 +0900

    trying solution2

--- linked_list/removeNthNodeFromTheEnd/my.py
+++ linked_list/removeNthNodeFromTheEnd/my.py
@@ -1,8 +1,11 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
         sz = 1
@@ -25,5 +28,36 @@ class Solution:
             else:
                 before.next = None
         return head
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution2:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        if head.next is None:
+            return head
+
+        vals = []
+        while head.next is not None:
+            vals.append(head.val)
+            head = head.next
+
+        target_idx = len(vals) - n
+        new_head = ListNode(val=None)
+        start = new_head
+        for idx in range(0, len(vals)):
+            if idx == target_idx:
+                continue
+            else:
+                if new_head.val is None:
+                    new_head = ListNode(val=vals[idx])
+                    start = new_head
+                else:
+                    new_head.next = ListNode(val=vals[idx])
+                    new_head = new_head.next
+        return start
 
 
