c781a55b8badc42353f370507b8d0bdcf38b23b4
Changjun Choi <changjun0823@gmail.com>
2023-10-23 08:27:23 +0900

    s1 study

--- nonClassified/GenerateParentheses/my.py
+++ nonClassified/GenerateParentheses/my.py
@@ -1,17 +1,17 @@
 from typing import List, Optional
 
 
-class Solution:
-    def generateParenthesis(self, n: int) -> List[str]:
-        count = n * 2
-        l, r = "(", ")"
-        sub = ""
-
-        def add_p(p: Optional[str]):
-            if not p:
-                p += l
-                return p
-            else:
-                if p[-1] == l:
-
-        return ans
+# class Solution:
+#     def generateParenthesis(self, n: int) -> List[str]:
+#         count = n * 2
+#         l, r = "(", ")"
+#         sub = ""
+#
+#         def add_p(p: Optional[str]):
+#             if not p:
+#                 p += l
+#                 return p
+#             else:
+#                 if p[-1] == l:
+#
+#         return ans
