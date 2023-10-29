e7f9bbdde7d7c21d8d16cf930ba0f0a01d098d9a
Changjun Choi <changjun0823@gmail.com>
2023-10-29 11:16:04 +0900

    thinking...

--- linked_list/SwapNodesInPairs/my.py
+++ linked_list/SwapNodesInPairs/my.py
@@ -6,15 +6,27 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
+    def __repr__(self):
+        return str(f"val = {self.val}, next = {self.next.val}")
 class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head or not head.next:
             return head
 
-        dummy = head.next
-        while head.next:
-            tmp = head.next
+        ans = ListNode(val=0, next=head.next)
+        while head and head.next:
+            dummy = head.next
             head.next = head.next.next
-            head = tmp.next
-            head = head.next.next
-        return dummy
+            dummy.next = head
+            head = dummy.next.next
+        return ans.next
+
+head = ListNode()
+dummy = head
+for i in range(1, 5):
+    n = ListNode(val=i)
+    head.next = n
+    head = head.next
+
+print(Solution().swapPairs(dummy.next))
